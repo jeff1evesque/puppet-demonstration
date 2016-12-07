4e4310904a68dc66119b5419ec9d3789e7c0dc8d
Jeff Levesque <jeff1evesque@yahoo.com>
2016-12-07 17:09:12 -0500

    #58: init.pp, change 'conf_dir' to 'root_dir'

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,11 +1,11 @@
 ## @init.pp, ensure puppet service enabled, and running on successive reboot.
 
 ## local variables
-$conf_dir = $::conf_dir
+$root_dir = $::conf_dir
 
 ## puppet service
 service { 'puppet':
     ensure    => running,
     enable    => true,
-    subscribe => File["${conf_dir}/puppet.conf"],
+    subscribe => File["${root_dir}/puppet.conf"],
 }
