4ed843e41901b8999e2711b35fd217314838b928
Jeff Levesque <jeff1evesque@yahoo.com>
2016-12-07 18:16:27 -0500

    #58: init.pp, define puppet service relationship

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,11 +1,21 @@
 ## @init.pp, ensure puppet service enabled, and running on successive reboot.
 
-## local variables
-$root_dir = $::conf_dir
+## ensure configuration: allows 'subscribe'
+##
+## @notify, restarts puppet if the file changes
+##
+file { '/etc/puppetlabs/puppet/puppet.conf':
+    ensure  => file,
+    content => template('puppet/puppet.erb'),
+    mode    => '0600',
+    owner   => 'root',
+    group   => 'root',
+    notify  => Service['puppet'],
+}
 
 ## puppet service
 service { 'puppet':
-    ensure    => running,
-    enable    => true,
-    subscribe => File["${root_dir}/puppet.conf"],
+    ensure  => running,
+    enable  => true,
+    require => File['/etc/puppetlabs/puppet/puppet.conf'],
 }
