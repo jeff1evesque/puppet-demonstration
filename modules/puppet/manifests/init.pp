73699f4f693501c16a57e8e608b57e6924cafe5c
Jeff Levesque <jeff1evesque@yahoo.com>
2016-12-08 07:03:15 -0500

    #58: init.pp, remove template

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,24 +1,7 @@
 ## @init.pp, ensure puppet service enabled, and running on successive reboot.
 
-## local variables
-$config_file = '/etc/puppetlabs/puppet/puppet.conf'
-
-## ensure configuration: allows 'subscribe'
-##
-## @notify, restarts puppet if the file changes
-##
-file { $config_file:
-    ensure  => file,
-    content => template('puppet/puppet.erb'),
-    mode    => '0600',
-    owner   => 'root',
-    group   => 'root',
-    notify  => Service['puppet'],
-}
-
 ## puppet service
 service { 'puppet':
     ensure  => running,
     enable  => true,
-    require => File[$config_file],
 }
